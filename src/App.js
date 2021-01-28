63ba4252b346ae315d5b0bb0532ddd2558a9e3f0
callmekeyz <callmekeyz@gmail.com>
2021-01-28 12:05:38 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './componets/Home';
+import About from './componets/About';
+import Blog from './componets/Blog';
+
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
       </header>
+
+      <Switch> 
+        <Route path="/"exact>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        
+       
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/componets/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Blog(props) {
+    return(
+        <h1>Blog</h1>
+    );
+}
+
+
+export default Blog;
